d1366a7b1d23cf45e9ed43aa2ad352bb48442ffb
Yaroslav <ipz214_dyav@student.ztu.edu.ua>
2024-08-13 14:07:27 +0300

    small schema improvements and seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 
 async function main() {
   const password = await hash('test', 12)
-  const user = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { email: 'test@test.com' },
     update: {},
     create: {
@@ -14,8 +14,51 @@ async function main() {
       password
     }
   })
-  console.log({ user })
+  await prisma.category.upsert({
+    where: { name: 'IT' },
+    update: {},
+    create: { name: 'IT' }
+  })
+
+  await prisma.category.upsert({
+    where: { name: 'Math' },
+    update: {},
+    create: { name: 'Math' }
+  })
+
+  await prisma.category.upsert({
+    where: { name: 'Soft skills' },
+    update: {},
+    create: { name: 'Soft skills' }
+  })
+
+  await prisma.category.upsert({
+    where: { name: 'Other' },
+    update: {},
+    create: { name: 'Other' }
+  })
+
+  await prisma.tag.upsert({
+    where: { name: 'Python' },
+    update: {},
+    create: { name: 'Python', color: '#003ab1' }
+  })
+
+  await prisma.tag.upsert({
+    where: { name: 'C#' },
+    update: {},
+    create: { name: 'C#', color: '#6600ff' }
+  })
+
+  await prisma.tag.upsert({
+    where: { name: 'JavaScript' },
+    update: {},
+    create: { name: 'JavaScript', color: '#ffae00' }
+  })
+
 }
+
+
 main()
   .then(() => prisma.$disconnect())
   .catch(async (e) => {
